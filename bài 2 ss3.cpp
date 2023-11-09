8e2d931178b9cc2f527cf432cefd6805b2f4814d
Hiendzklo1 <147359328+Hiendzklo1@users.noreply.github.com>
2023-11-09 07:49:14 +0700

    Update bài 2 ss3.cpp

--- bài 2 ss3.cpp
+++ bài 2 ss3.cpp
@@ -2,7 +2,7 @@
 int main(){
 	printf(" stt  Ho va Ten       Age     so dien thoai    email                 \n");
 	printf(" 1    Nguyen Van A    20      0988875789     anc@rikkeiacademy.com \n");
-    printf(" 2    Nguyen Van B    22      0988875734     rew@rikkeiacademy.com \n");
+        printf(" 2    Nguyen Van B    22      0988875734     rew@rikkeiacademy.com \n");
 	printf(" 3    Nguyen Van C    19      0535666662     sdf@rikkeiacademy.com \n");
 	printf(" 4    Nguyen Van D    24      0983577442     adf@rikkeiacademy.com \n");
 	printf(" 5    Nguyen Van E    23      0257684324     khl@rikkeiacademy.com \n");
